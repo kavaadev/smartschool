b8f3ef2380f600dcce48215ec173592a6d06988f
Othmane FARAJ <oth.faraj@gmail.com>
2016-03-30 14:35:51 +0100

    ok

--- scripts/app/main/main.controller.js
+++ scripts/app/main/main.controller.js
@@ -5,23 +5,14 @@ angular.module('smartschoolApp')
 
 
 
-        $scope.mesecoles = [];
-        $scope.ecoles = Ecole.query({size:1000});
+       $scope.mesecoles = [{
+               id : "1",
+               intitule : "Ecole de test"
+       }];
 
-        Principal.identity().then(function(account) {
-            $scope.account = account;
-            $scope.isAuthenticated = Principal.isAuthenticated;
-
-            $scope.ecoles = Ecole.query(function (response) {
-                                            angular.forEach(response, function (item) {
-                                                    if(item.user){
-                                                    if(item.user.login == $scope.account.login){
-                                                        $scope.mesecoles.push(item);
-                                                    }}
-
-                                            });
-                                        });
-
-        });
+       $scope.isAuthenticated = false;
+       $scope.account = { username : "directeur",
+                          password : "directeur"
+       };
 
     });
--- scripts/components/navbar/navbar.controller.js
+++ scripts/components/navbar/navbar.controller.js
@@ -3,31 +3,24 @@
 angular.module('smartschoolApp')
     .controller('NavbarController', function ($scope, $location, $state, Auth, Principal, ENV,Ecole) {
 
-        $scope.mesecoles = [];
-        $scope.ecoles = Ecole.query({size:1000});
-        $scope.isAuthenticated = Principal.isAuthenticated;
+        $scope.mesecoles = [{
+                                id : "1",
+                                intitule : "Ecole de test"
+                            }];
+
+        $scope.isAuthenticated = false;
         $scope.$state = $state;
         $scope.inProduction = ENV === 'prod';
+
         $scope.logout = function () {
-            Auth.logout();
             $state.go('login');
         };
 
-        Principal.identity().then(function(account) {
-                    $scope.account = account;
-                    $scope.isAuthenticated = Principal.isAuthenticated;
+        $scope.account = { username : "directeur",
+                                       password : "directeur"
+        };
 
-
-                    $scope.ecoles = Ecole.query(function (response) {
-                                angular.forEach(response, function (item) {
-                                        if(item.user){
-                                        if(item.user.login == $scope.account.login){
-                                            $scope.mesecoles.push(item);
-                                        }}
-
-                                });
-                            });
-                });
+        $scope.isAuthenticated = false;
 
 
 
